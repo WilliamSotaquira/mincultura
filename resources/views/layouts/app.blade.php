58e7010ba994012943c1ee5f4297b8b908d7f77b
WilliamSotaquira <william.sotaquira@gmail.com>
2026-02-16 16:12:05 -0500

    Integra CDN govco/bootstrap y elimina carga Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,5 +11,13 @@
     </main>
 
     @include('partials.footer')
+
+    <!-- utils.js BDC -->
+    <script src="https://cdn.www.gov.co/layout-govco-v5/script.js"></script>
+
+    <!-- js bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,4 +1,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>@yield('title', config('app.name', 'Mincultura'))</title>
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+<!-- css BDC -->
+<link href="https://cdn.www.gov.co/layout-govco-v5/all.css" rel="stylesheet">
+
+<!-- css bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      rel="stylesheet" crossorigin="anonymous">
